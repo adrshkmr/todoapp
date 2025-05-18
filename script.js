d0d88a11f02ca7983b7d71422e0f79de6aec38d7
adrshkmr <education@erased.aleeas.com>
2025-05-18 19:10:30 +0530

    added functionality to add task, delete task and mark done

--- script.js
+++ script.js
@@ -2,12 +2,56 @@ const leftPanel = document.querySelector('#leftPanel');
 const rightPanel = document.querySelector('#RightPanel');
 const taskForm = document.querySelector('#taskForm');
 const taskList = document.querySelector('#taskList');
+const taskInput = document.querySelector('#textArea');
 
 
 
 taskForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const formData = new FormData(taskForm);
-    console.log(formData.entries());
+    let taskText = taskInput.value.trim();
+
+    if(taskText === "") return;
+
+    //add tasks as list
+    const listItem = document.createElement('li');
+    listItem.textContent = taskText;
+
+    //create delete button
+    const delBtn = document.createElement('button');
+    delBtn.type = 'button';
+    delBtn.textContent = '⚔️';
+    delBtn.classList.add('del-button');
+    delBtn.addEventListener('click', () => {
+        taskList.removeChild(listItem);
+    })
+
+
+    //mark done feature
+    const doneBtn = document.createElement('button');
+    doneBtn.type = 'button';
+    doneBtn.classList.add('done-button');
+    doneBtn.textContent = '✔️';
+    doneBtn.addEventListener('click', () => {
+        listItem.style.textDecoration = 'line-through';
+    })
+
+
+
+    taskList.appendChild(listItem);
+    listItem.appendChild(delBtn);
+    listItem.appendChild(doneBtn);
+
+
+    taskInput.value = '';
+})
+
+
+
+//submit on enter
+taskForm.addEventListener('keydown', (e) => {
+    if(e.key == 'Enter'){
+        e.preventDefault();
+        taskForm.requestSubmit();
+    }
 })
